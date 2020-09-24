c8e8e16f2bc59d9fde321a72e03ca57709192584
nick momotenko <nickmomotenkoforweb@gmail.com>
2020-09-24 19:43:01 +0300

    Updates

---
+++ precache-manifest.93f6ed4921809b754d8ab56ac20cd5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e8d797219da418b8f4749af0cf8231",
+    "url": "/react-ultimate-form/index.html"
+  },
+  {
+    "revision": "dd9e5549303ddf54ba28",
+    "url": "/react-ultimate-form/static/css/main.eb5a1001.chunk.css"
+  },
+  {
+    "revision": "929b37ebd7485af068ee",
+    "url": "/react-ultimate-form/static/js/2.7cf3a5b9.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react-ultimate-form/static/js/2.7cf3a5b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9e5549303ddf54ba28",
+    "url": "/react-ultimate-form/static/js/main.884b634c.chunk.js"
+  },
+  {
+    "revision": "12246d6fe5d19f7dde8f",
+    "url": "/react-ultimate-form/static/js/runtime-main.6286e004.js"
+  }
+]);
